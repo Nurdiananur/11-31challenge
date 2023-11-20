63e862ca7d3334fd9a66d62b9c390e31f222b205
Nurdiana <84701259+Nurdiananur@users.noreply.github.com>
2023-11-20 13:58:52 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'fdssgsgfsdgd',
+              textDirection: TextDirection.ltr,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
